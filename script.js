5e529d9688be4cf7f9c2facec391bd25ed7eeeba
Monalize M <150396638+mlize08@users.noreply.github.com>
2025-07-01 21:14:56 -0300

    listas

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function inserirNome(){
+function inserirNome()  {
     let nomeUsuario = prompt("Qual o seu nome?");
     let elemento = document.querySelector("#nome-usuario");
     elemento.textContent = nomeUsuario;
 }
 inserirNome();
+
+let linguagens = ["Javascript", "Python", "C"];
+// indice      
+console.log(linguagens);       
+const item = document.querySelector("#lista");
+item.textContent = linguagens[1];
+item.textContent = linguagens[2];
+item.textContent = linguagens[0];
