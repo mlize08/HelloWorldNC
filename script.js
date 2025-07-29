bba01b47a5a41a36de8e7362c5316e066a1f1dba
Monalize M <150396638+mlize08@users.noreply.github.com>
2025-07-29 20:14:50 -0300

    s

--- script.js
+++ script.js
@@ -5,10 +5,25 @@ function inserirNome()  {
 }
 inserirNome();
 
-let linguagens = ["Javascript", "Python", "C"];
+const linguagens = ["Javascript", "Python", "C"];
 // indice      
-console.log(linguagens);       
+linguagens[2] = "HTML";
+console.log(linguagens[2]);       
 const item = document.querySelector("#lista");
 item.textContent = linguagens[1];
 item.textContent = linguagens[2];
 item.textContent = linguagens[0];
+
+let aluno1 = {
+  nome: "Gabriel",
+  idade: 13,
+  anoLetivo: "2ºsérie",
+  materiasFavoritas: ["Geografia","Programação","Arte"]
+}
+
+console.log(aluno1.nome); // “Gabriel”
+console.log(aluno1.idade); // 13
+console.log(aluno1.materiasFavoritas);
+// ["Geografia","Programação","Arte"]
+console.log(aluno1.materiasFavoritas[1]);
+
