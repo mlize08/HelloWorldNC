7f0974b98446de7968a7b01f8d51eca514d07310
Monalize M <150396638+mlize08@users.noreply.github.com>
2025-08-12 22:17:19 -0300

    s

--- script.js
+++ script.js
@@ -10,9 +10,18 @@ const linguagens = ["Javascript", "Python", "C"];
 linguagens[2] = "HTML";
 console.log(linguagens[2]);       
 const item = document.querySelector("#lista");
+
 item.textContent = linguagens[1];
 item.textContent = linguagens[2];
 item.textContent = linguagens[0];
+
+//Solução 2: Usar laço de repetição "for of"
+for (const linguagem of linguagens) {
+  const itemDaLista = document.createElement("li");
+  itemDaLista.textContent = linguagem;
+  item.appendChild(itemDaLista); // "item" foi criada mais acima
+}
+
 
 let aluno1 = {
   nome: "Gabriel",
@@ -30,3 +39,5 @@ console.log(aluno1.materiasFavoritas[1]);
 const objeto = document.querySelector("#aluno");
 objeto.textContent = aluno1.nome;
 objeto.textContent = aluno1.idade;
+objeto.textContent = aluno1.materiasFavoritas;
+objeto.textContent = aluno1.materiasFavoritas[2];
